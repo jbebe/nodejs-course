423e2973de19b4be0129653d2919ed924f67a888
jbebe <juhasz.balint.bebe@gmail.com>
2018-03-30 00:15:00 +0200

    tasks page is now handled by code

--- middlewares/render.js
+++ middlewares/render.js
@@ -70,7 +70,30 @@ exports.RenderTasksMW = function(req, res, next) {
   const template = {
     title: `${CourseCode}/tasks`,
     courseCode: CourseCode,
-    currentPage: 'tasks'
+    currentPage: 'tasks',
+    tasks: [
+      {
+        name: 'Alapok',
+        deadline: (new Date()).toJSON().slice(0,-5),
+        id: 1,
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the\n' +
+        'industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and\n' +
+        'scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into\n' +
+        'electronic typesetting, remaining essentially unchanged.'
+      },
+      {
+        name: 'Fejlesztés',
+        deadline: (new Date()).toJSON().slice(0,-5),
+        id: 2,
+        description: 'Fejlessz'
+      },
+      {
+        name: 'Végleges',
+        deadline: (new Date()).toJSON().slice(0,-5),
+        id: 3,
+        description: 'Add be.'
+      }
+    ]
   };
   Object.assign(res.locals.template, template);
   res.render('tasks', res.locals.template);
